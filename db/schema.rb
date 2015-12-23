f648969c7eb43b9ecffa836e672a8b7c3b1deabf
emanuelhailu <emparalhailu@yahoo.com>
2015-12-23 15:49:59 +0300

    Added a Note Controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151223124853) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
